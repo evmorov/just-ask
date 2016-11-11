c26dfc86ac0a4a6ebedbbd0f8cc579de4461d4a4
Evgeny Morozov <evmorov@gmail.com>
2016-11-11 13:30:14 +0300

    Use add_belongs_to that is more approptiate than add_reference

--- db/migrate/20161110200326_add_question_to_answers.rb
+++ db/migrate/20161110200326_add_question_to_answers.rb
@@ -1,5 +1,5 @@
 class AddQuestionToAnswers < ActiveRecord::Migration[5.0]
   def change
-    add_reference :answers, :question, foreign_key: true
+    add_belongs_to :answers, :question, foreign_key: true
   end
 end
