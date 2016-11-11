d5cacbdcfdda9a2900ad90c0e37b5d408c62d44a
Evgeny Morozov <evmorov@gmail.com>
2016-11-11 13:33:29 +0300

    Indexes don't need to be tests

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,5 +5,4 @@ RSpec.describe Answer, type: :model do
 
   it { should validate_presence_of(:body) }
   it { should validate_length_of(:body).is_at_least(5) }
-  it { should have_db_index(:question_id) }
 end
