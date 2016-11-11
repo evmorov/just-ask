123c4024c6c518d44b0163bc7fe0988999cc19e3
Evgeny Morozov <evmorov@gmail.com>
2016-11-11 13:02:00 +0300

    Don't validate acossiations - Rails 5 does it for you

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should validate_presence_of(:body) }
-  it { should validate_presence_of(:question_id) }
   it { should belong_to(:question) }
   it { should validate_length_of(:body).is_at_least(5) }
   it { should have_db_index(:question_id) }
