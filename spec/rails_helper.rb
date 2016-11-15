ed00dc2130a3bf23e5746dc5768f864205cbe0d9
Evgeny Morozov <evmorov@gmail.com>
2016-11-15 23:57:26 +0300

    Fix running RSpec without Spring

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,11 +4,13 @@ require File.expand_path('../../config/environment', __FILE__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'factory_girl_rails'
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
   config.include AcceptanceMacros, type: :feature
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
