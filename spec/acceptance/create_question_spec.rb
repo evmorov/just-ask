5f9b8915a5c2ebfeb7ff69c764fa3386cf8037a2
Evgeny Morozov <evmorov@gmail.com>
2016-11-17 00:54:39 +0300

    Feature: only authenticated use can ask a question

--- spec/acceptance/create_question_spec.rb
+++ spec/acceptance/create_question_spec.rb
@@ -6,7 +6,17 @@ feature 'Create a question', '
   I want to be able to ask a question
 ' do
 
+  given(:user) { create(:user) }
+
+  scenario 'Create a question when authenticated' do
+    visit questions_path
+    click_on 'Ask Question'
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+
   scenario 'Create a question when non-authenticated' do
+    sign_in(user)
     visit questions_path
     click_on 'Ask Question'
     fill_in 'Title', with: 'Test question'
