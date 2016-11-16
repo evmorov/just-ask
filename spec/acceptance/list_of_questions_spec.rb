19a41855be32671e76ddb4df65e54ee9245f80a9
Evgeny Morozov <evmorov@gmail.com>
2016-11-16 10:08:39 +0300

    Add feature: view list of questions

---
+++ spec/acceptance/list_of_questions_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'List of questions', '
+  In order to find a question
+  As a user
+  I want to be able to view all questions
+' do
+
+  before do
+    create(:question, title: 'First')
+    create(:question, title: 'Second')
+  end
+
+  scenario 'Non-registered user can see a list of questions titles' do
+    visit questions_path
+
+    expect(page).to have_link('First')
+    expect(page).to have_link('Second')
+  end
+end
