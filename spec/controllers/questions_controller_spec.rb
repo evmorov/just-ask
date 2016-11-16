d3f20a5a631cbd99af3f660b57d7dae44a513d1a
Evgeny Morozov <evmorov@gmail.com>
2016-11-16 10:08:39 +0300

    Add feature: view list of questions

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,6 +3,20 @@ require 'rails_helper'
 describe QuestionsController, type: :controller do
   let(:question) { create(:question) }
 
+  describe 'GET #index' do
+    let(:questions) { create_list(:question, 2) }
+
+    before { get :index }
+
+    it 'loads all of the questions into @questions' do
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template(:index)
+    end
+  end
+
   describe 'GET #show' do
     before { get :show, params: { id: question } }
 
