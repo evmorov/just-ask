77813da111e68c88ce005b2200527948d65f5e71
Evgeny Morozov <evmorov@gmail.com>
2016-11-16 23:47:26 +0300

    Add feature for signing out

---
+++ spec/acceptance/sign_out_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'Signing out', '
+  In order to be more secure
+  As an authenticated user
+  I want be able to sign out
+' do
+
+  given(:user) { create(:user) }
+
+  scenario 'Sign out' do
+    sign_in(user)
+    click_on 'Sign out'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
