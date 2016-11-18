137ececb7de8aa70d710d73e78a7df90d41ebc72
Evgeny Morozov <evmorov@gmail.com>
2016-11-18 23:22:34 +0300

    Add tests for validation User fields

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 describe User, type: :model do
   it { should have_many(:questions).dependent(:destroy) }
   it { should have_many(:answers).dependent(:destroy) }
+
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :password }
 end
