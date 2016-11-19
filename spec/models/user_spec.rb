a6b5b379ea3eba5665dff4932b58c2d8020c5864
Evgeny Morozov <evmorov@gmail.com>
2016-11-19 22:28:34 +0300

    Add tests for #author_of

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,23 @@ describe User, type: :model do
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  describe '#author_of' do
+    let(:user) { create(:user) }
+
+    it 'returns true if an argument belongs to User' do
+      question = create(:question, user: user)
+      expect(user).to be_author_of(question)
+      # expect(user.author_of?(question)).to be(true)
+    end
+
+    it 'returns false if an argument from another User' do
+      question = create(:question)
+      expect(user).to_not be_author_of(question)
+    end
+
+    it 'raises NoMethodError if an argument does not repond to #user_id' do
+      expect { user.author_of? [] }.to raise_error(NoMethodError)
+    end
+  end
 end
