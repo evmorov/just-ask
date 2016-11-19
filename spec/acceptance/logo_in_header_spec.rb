87c3b407fbf1fdbd60f1c20da8b8f2d97fb60650
Evgeny Morozov <evmorov@gmail.com>
2016-11-19 23:59:18 +0300

    Add header

---
+++ spec/acceptance/logo_in_header_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'Logo in the header', '
+  In order to go to the root page
+  As a user
+  I want to be able click the logo
+' do
+
+  given(:question) { create(:question) }
+
+  scenario 'Click the logo from Question page' do
+    visit question_path(question)
+    click_on 'logo'
+
+    expect(page).to have_current_path(root_path)
+  end
+end
