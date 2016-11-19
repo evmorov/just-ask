1bb5572cf27a2e2f931bb26e428762948b1a01cb
Evgeny Morozov <evmorov@gmail.com>
2016-11-20 00:58:36 +0300

    Fix typo

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -62,7 +62,7 @@ describe AnswersController, type: :controller do
 
       before { answer }
 
-      it 'dot not deletes answer' do
+      it 'does not delete answer' do
         expect {
           delete :destroy, params: { id: answer }
         }.to_not change(Answer, :count)
