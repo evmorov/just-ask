6ed8d27c900a189d7045854380c9062277ea4825
Evgeny Morozov <evmorov@gmail.com>
2016-11-21 00:38:04 +0300

    Use ajax when posting answers

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -9,32 +9,32 @@ describe AnswersController, type: :controller do
     context 'with valid attributes' do
       it 'saves the new answer in the database' do
         expect {
-          post :create, params: { answer: attributes_for(:answer), question_id: question }
+          post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js }
         }.to change(question.answers, :count).by(1)
       end
 
       it 'saves the new answer with the current user' do
         expect {
-          post :create, params: { answer: attributes_for(:answer), question_id: question }
+          post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js }
         }.to change(subject.current_user.answers, :count).by(1)
       end
 
-      it 'redirects to show the question' do
-        post :create, params: { answer: attributes_for(:answer), question_id: question }
-        expect(response).to redirect_to question
+      it 'render create template' do
+        post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js }
+        expect(response).to render_template :create
       end
     end
 
     context 'with invalid attributes' do
       it 'does not save the answer' do
         expect {
-          post :create, params: { answer: attributes_for(:invalid_answer), question_id: question }
+          post :create, params: { answer: attributes_for(:invalid_answer), question_id: question, format: :js }
         }.to_not change(Answer, :count)
       end
 
-      it 're-renders show question view' do
-        post :create, params: { answer: attributes_for(:invalid_answer), question_id: question }
-        expect(response).to render_template('questions/show')
+      it 'render create template' do
+        post :create, params: { answer: attributes_for(:invalid_answer), question_id: question, format: :js }
+        expect(response).to render_template :create
       end
     end
   end
