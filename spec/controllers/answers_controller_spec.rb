5cdf5d62575aee0c3b720b84fd3d808ee6cf5394
Evgeny Morozov <evmorov@gmail.com>
2016-11-21 22:54:11 +0300

    Refactor to make line length shorter

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -9,18 +9,24 @@ describe AnswersController, type: :controller do
     context 'with valid attributes' do
       it 'saves the new answer in the database' do
         expect {
-          post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js }
+          post :create, params: {
+            answer: attributes_for(:answer), question_id: question, format: :js
+          }
         }.to change(question.answers, :count).by(1)
       end
 
       it 'saves the new answer with the current user' do
         expect {
-          post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js }
+          post :create, params: {
+            answer: attributes_for(:answer), question_id: question, format: :js
+          }
         }.to change(subject.current_user.answers, :count).by(1)
       end
 
       it 'render create template' do
-        post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js }
+        post :create, params: {
+          answer: attributes_for(:answer), question_id: question, format: :js
+        }
         expect(response).to render_template :create
       end
     end
@@ -28,12 +34,16 @@ describe AnswersController, type: :controller do
     context 'with invalid attributes' do
       it 'does not save the answer' do
         expect {
-          post :create, params: { answer: attributes_for(:invalid_answer), question_id: question, format: :js }
+          post :create, params: {
+            answer: attributes_for(:invalid_answer), question_id: question, format: :js
+          }
         }.to_not change(Answer, :count)
       end
 
       it 'render create template' do
-        post :create, params: { answer: attributes_for(:invalid_answer), question_id: question, format: :js }
+        post :create, params: {
+          answer: attributes_for(:invalid_answer), question_id: question, format: :js
+        }
         expect(response).to render_template :create
       end
     end
