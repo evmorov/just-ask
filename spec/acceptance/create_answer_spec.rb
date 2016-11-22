aed4841a5f9c452161c705fc36cd169e6980e3db
Evgeny Morozov <evmorov@gmail.com>
2016-11-22 23:59:00 +0300

    Show a warning if a question has errors

--- spec/acceptance/create_answer_spec.rb
+++ spec/acceptance/create_answer_spec.rb
@@ -9,22 +9,35 @@ feature 'Create an answer', '
   given(:question) { create(:question) }
   given(:user) { create(:user) }
 
-  scenario 'Create an answer when authenticated', js: true do
-    sign_in(user)
-    visit question_path(question)
-    fill_in 'answer_body', with: 'My smart answer'
-    click_on 'Create Answer'
+  context 'When authenticated' do
+    before do
+      sign_in(user)
+      visit question_path(question)
+    end
 
-    within('.answers') do
-      expect(page).to have_content 'My smart answer'
+    scenario 'Create an answer when authenticated', js: true do
+      fill_in 'answer_body', with: 'My smart answer'
+      click_on 'Create Answer'
+
+      within('.answers') do
+        expect(page).to have_content 'My smart answer'
+      end
+    end
+
+    scenario 'Create an invalid answer when non-authenticated', js: true do
+      click_on 'Create Answer'
+
+      expect(page).to have_content "Body can't be blank"
     end
   end
 
-  scenario 'Create an answer when non-authenticated' do
-    visit question_path(question)
-    fill_in 'answer_body', with: 'My smart answer'
-    click_on 'Create Answer'
+  context 'When non-authenticated' do
+    scenario 'Create an answer' do
+      visit question_path(question)
+      fill_in 'answer_body', with: 'My smart answer'
+      click_on 'Create Answer'
 
-    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    end
   end
 end
