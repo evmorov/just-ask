734587b933e8c12250f9641afd06c8e2a88372e8
Evgeny Morozov <evmorov@gmail.com>
2016-11-23 23:58:02 +0300

    Fix removing an answer with ajax after conflict

--- spec/acceptance/remove_answer_spec.rb
+++ spec/acceptance/remove_answer_spec.rb
@@ -23,7 +23,8 @@ feature 'Remove an answer', '
 
     scenario 'Remove my own answer', js: true do
       within('div', text: my_answer.body, class: 'answer') do
-        page.accept_confirm click_on 'Remove'
+        page.driver.browser.accept_js_confirms
+        click_on 'Remove'
       end
 
       expect(page).to_not have_content my_answer.body
