3698d9d0ceb6bb3bda56e4f785d33cc87e5db9ef
Evgeny Morozov <evmorov@gmail.com>
2016-11-28 23:32:02 +0300

    Use update instead of save

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -9,13 +9,8 @@ class Answer < ApplicationRecord
   def toggle_best
     transaction do
       the_best_answer = question.answers.find_by(best: true)
-      if the_best_answer
-        the_best_answer.best = false
-        the_best_answer.save
-      end
-
-      self.best = !best
-      save
+      the_best_answer.update(best: false) if the_best_answer
+      update(best: !best)
     end
   end
 end
