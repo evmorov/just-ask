bdb65d9a68f7d6cf19e8f1a8f2dc91a0ee4e6a2b
Evgeny Morozov <evmorov@gmail.com>
2016-11-28 23:20:20 +0300

    Move the best answer logic from controller to model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,4 +5,15 @@ class Answer < ApplicationRecord
   validates :body, presence: true, length: { minimum: 5 }
 
   default_scope { order(:created_at) }
+
+  def toggle_best
+    the_best_answer = question.answers.find(&:best)
+    if the_best_answer
+      the_best_answer.best = false
+      the_best_answer.save
+    end
+
+    self.best = !best
+    save
+  end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -6,4 +6,34 @@ describe Answer, type: :model do
 
   it { should validate_presence_of(:body) }
   it { should validate_length_of(:body).is_at_least(5) }
+
+  describe '#best_answer' do
+    let(:question) { create(:question) }
+    let(:answer) { create(:answer, question: question, best: false) }
+    let!(:best_answer) { create(:answer, question: question, best: true) }
+
+    it 'toggles the best attribute' do
+      answer.toggle_best
+      expect(answer.best).to be(true)
+
+      answer.toggle_best
+      expect(answer.best).to be(false)
+    end
+
+    it 'set the best attribute and if there is another best answer makes it false' do
+      answer.toggle_best
+      best_answer.reload
+
+      expect(answer.best).to be(true)
+      expect(best_answer.best).to be(false)
+    end
+
+    it 'unselects the best answer if it is already the best' do
+      best_answer.toggle_best
+      answer.reload
+
+      expect(best_answer.best).to be(false)
+      expect(answer.best).to be(false)
+    end
+  end
 end
