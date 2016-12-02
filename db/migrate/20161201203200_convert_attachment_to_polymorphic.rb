728c54ee36a77af20092422d805ab4722014fb02
Evgeny Morozov <evmorov@gmail.com>
2016-12-02 23:28:39 +0300

    Fix migrations

--- db/migrate/20161201203200_convert_attachment_to_polymorphic.rb
+++ db/migrate/20161201203200_convert_attachment_to_polymorphic.rb
@@ -1,6 +1,8 @@
 class ConvertAttachmentToPolymorphic < ActiveRecord::Migration[5.0]
   def change
     remove_reference :attachments, :question, foreign_key: true
+
+    add_column :attachments, :attachable_id, :integer
     add_column :attachments, :attachable_type, :string
     add_index :attachments, [:attachable_id, :attachable_type]
   end
