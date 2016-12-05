c3bb37d21a4e023c0e67a3b1cb1c0bff2d752594
Evgeny Morozov <evmorov@gmail.com>
2016-12-05 23:40:40 +0300

    Refactor factory

--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :attachment do
-    file { File.new(Rails.root.join('spec', 'spec_helper.rb')) }
+    file { File.new "#{Rails.root}/spec/spec_helper.rb" }
   end
 end
