7209d00bb28314cb116bbdf1321ee4953f334500
Evgeny Morozov <evmorov@gmail.com>
2016-12-06 00:12:56 +0300

    Handle right on attachments

--- spec/acceptance/files/remove_files_spec.rb
+++ spec/acceptance/files/remove_files_spec.rb
@@ -6,22 +6,79 @@ feature 'Remove a file', '
   I want to be able to remove files
 ' do
 
-  context 'When authenticated' do
-    given(:user) { create(:user) }
-    given(:question) { create(:question, user: user) }
+  given(:user) { create(:user) }
+  given(:question) { create(:question, user: user) }
+  given(:answer) { create(:answer, user: user, question: question) }
+  given!(:attach_question) { create(:attachment, attachable: question) }
+  given!(:attach_answer) { create(:attachment, attachable: answer) }
 
+  given(:another_user) { create(:user) }
+
+  context 'When authenticated' do
+    context 'remove my own attachments' do
+      background do
+        sign_in(user)
+        visit question_path(question)
+      end
+
+      scenario 'remove a file for my own question', js: true do
+        within('#question') do
+          find(:css, '.remove-attachment-link').trigger('click')
+
+          expect(page).to_not have_link(
+            attach_question.file.identifier,
+            href: attach_question.file.url
+          )
+        end
+      end
+
+      scenario 'remove a file for my own answer', js: true do
+        within('#answers') do
+          find(:css, '.remove-attachment-link').trigger('click')
+
+          expect(page).to_not have_link(
+            attach_question.file.identifier,
+            href: attach_question.file.url
+          )
+        end
+      end
+    end
+
+    context "another user can't remove my attachments" do
+      background do
+        sign_in(another_user)
+        visit question_path(question)
+      end
+
+      scenario 'no remove icon for my question' do
+        within('#question') do
+          expect(page).to_not have_css('.remove-attachment-link')
+        end
+      end
+
+      scenario 'no remove icon for my answer' do
+        within('#answers') do
+          expect(page).to_not have_css('.remove-attachment-link')
+        end
+      end
+    end
+  end
+
+  context 'When unauthenticated' do
     background do
-      sign_in(user)
       visit question_path(question)
     end
 
-    scenario 'remove a file for my own answer', js: true do
-      fill_in 'Add answer', with: 'My smart answer'
-      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-      click_on 'Create Answer'
-      find(:css, '.remove-attachment-link').trigger('click')
+    scenario 'do not see a remove icon for a question' do
+      within('#question') do
+        expect(page).to_not have_css('.remove-attachment-link')
+      end
+    end
 
-      expect(page).to_not have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    scenario 'do not see a remove icon for an answer' do
+      within('#answers') do
+        expect(page).to_not have_css('.remove-attachment-link')
+      end
     end
   end
 end
