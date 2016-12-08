ae5a8571cbe63ef640b11f0ea0184d86c4fe7153
Evgeny Morozov <evmorov@gmail.com>
2016-12-09 01:24:21 +0300

    Refactor tests to use shared example on attachments

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 describe Question, type: :model do
+  it_behaves_like 'attachable'
+
   it { should belong_to(:user) }
   it { should have_many(:answers).dependent(:destroy) }
-  it { should have_many(:attachments) }
 
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
   it { should validate_length_of(:title).is_at_least(5) }
   it { should validate_length_of(:body).is_at_least(5) }
-
-  it { should accept_nested_attributes_for :attachments }
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'factory_girl_rails'
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/concerns/**/*.rb')].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
