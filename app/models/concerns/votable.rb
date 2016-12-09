b2aec3af8fd4d06b222d3549169bc9340deaecd9
Evgeny Morozov <evmorov@gmail.com>
2016-12-09 23:44:01 +0300

    User has many votes

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -2,7 +2,9 @@ module Votable
   extend ActiveSupport::Concern
 
   included do
+    belongs_to :user
     has_many :votes, as: :votable
+
     accepts_nested_attributes_for :votes, reject_if: :all_blank, allow_destroy: true
   end
 end
