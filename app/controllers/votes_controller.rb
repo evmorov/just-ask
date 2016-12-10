fb1093fe4f9f8abf2c90b277fa5398216aec157c
Evgeny Morozov <evmorov@gmail.com>
2016-12-10 16:46:29 +0300

    Wip on votes

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,28 @@
+class VotesController < ApplicationController
+  before_action :authenticate_user!
+
+  def upvote
+    votable = find_votable
+
+    vote = Vote.find_by(votable: votable, user: current_user)
+    if vote # already voted
+      vote.destroy
+    else
+      votable.votes.create(score: 1, user: current_user)
+    end
+  end
+
+  def downvote
+  end
+
+  private
+
+  def find_votable
+    params.each do |name, value|
+      if name =~ /(.+)_id$/
+        return $1.classify.constantize.find(value)
+      end
+    end
+    nil
+  end
+end
