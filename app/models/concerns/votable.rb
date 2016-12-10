52ee2b648f8d400456e8a82698b90c4491b0783a
Evgeny Morozov <evmorov@gmail.com>
2016-12-11 01:53:13 +0300

    Move vote logic to from Vote to Votable

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -7,4 +7,30 @@ module Votable
 
     accepts_nested_attributes_for :votes, reject_if: :all_blank, allow_destroy: true
   end
+
+  def upvote(params)
+    give_vote(1, params)
+  end
+
+  def downvote(params)
+    give_vote(-1, params)
+  end
+
+  private
+
+  def give_vote(score, params)
+    transaction do
+      user_id = params[:user_id]
+      vote_same_attrs = Vote.find_by(votable: self, user_id: user_id)
+
+      if vote_same_attrs
+        if vote_same_attrs.score != score
+          vote_same_attrs.destroy!
+          votes.create!(score: score, user_id: user_id)
+        end
+      else
+        votes.create!(score: score, user_id: user_id)
+      end
+    end
+  end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,31 +1,4 @@
 class Vote < ApplicationRecord
   belongs_to :votable, polymorphic: true
   belongs_to :user
-
-  def self.upvote(params)
-    give_vote(params, 1)
-  end
-
-  def self.downvote(params)
-    give_vote(params, -1)
-  end
-
-  private
-
-  def self.give_vote(params, score)
-    transaction do
-      user = params[:user]
-      votable = params[:votable]
-      vote_same_attrs = Vote.find_by(votable: votable, user: user)
-
-      if vote_same_attrs
-        if vote_same_attrs.score != score
-          vote_same_attrs.destroy!
-          votable.votes.create!(score: score, user: user)
-        end
-      else
-        votable.votes.create!(score: score, user: user)
-      end
-    end
-  end
 end
