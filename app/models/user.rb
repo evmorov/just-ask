b5a0dbcda6a27d5f5c552fd841b64798ccff80dc
Evgeny Morozov <evmorov@gmail.com>
2017-01-13 00:24:29 +0300

    Use transaction

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,18 +14,20 @@ class User < ApplicationRecord
   end
 
   def self.find_for_oauth(auth)
-    authorization = Authorization.find_by(provider: auth.provider, uid: auth.uid.to_s)
-    return authorization.user if authorization
+    transaction do
+      authorization = Authorization.find_by(provider: auth.provider, uid: auth.uid.to_s)
+      return authorization.user if authorization
 
-    email = auth.info.email
-    user = User.find_by(email: email)
+      email = auth.info.email
+      user = User.find_by(email: email)
 
-    unless user
-      password = Devise.friendly_token[0, 20]
-      user = User.create!(email: email, password: password, password_confirmation: password)
+      unless user
+        password = Devise.friendly_token[0, 20]
+        user = User.create!(email: email, password: password, password_confirmation: password)
+      end
+
+      user.authorizations.create(provider: auth.provider, uid: auth.uid)
+      user
     end
-
-    user.authorizations.create(provider: auth.provider, uid: auth.uid)
-    user
   end
 end
