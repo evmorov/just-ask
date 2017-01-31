94f2a7614721dcf8e6ac7f2d801beb613753ed17
Evgeny Morozov <evmorov@gmail.com>
2017-02-01 01:00:33 +0300

    Fix warning because of Rails 5

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -4,12 +4,12 @@ describe 'Profile API' do
   describe 'GET /me' do
     context 'unauthorized' do
       it 'returns 401 status if there is no access_token' do
-        get '/api/v1/profiles/me', format: :json
+        get '/api/v1/profiles/me', params: { format: :json }
         expect(response.status).to eq 401
       end
 
       it 'returns 401 status if access_token is invalid' do
-        get '/api/v1/profiles/me', format: :json, access_token: '1234'
+        get '/api/v1/profiles/me', params: { format: :json, access_token: '1234' }
         expect(response.status).to eq 401
       end
     end
@@ -18,7 +18,9 @@ describe 'Profile API' do
       let(:me) { create(:user) }
       let(:access_token) { create(:access_token, resource_owner_id: me.id) }
 
-      before { get '/api/v1/profiles/me', format: :json, access_token: access_token.token }
+      before do
+        get '/api/v1/profiles/me', params: { format: :json, access_token: access_token.token }
+      end
 
       it 'returns 200 status' do
         expect(response).to be_success
