3e81ae739a1cca022ec8b39ec126a81a0d0f93b6
Evgeny Morozov <evmorov@gmail.com>
2017-02-05 01:10:41 +0300

    Move vote acceptance specs to shared

--- spec/acceptance/votes/vote_on_question_spec.rb
+++ spec/acceptance/votes/vote_on_question_spec.rb
@@ -11,123 +11,22 @@ feature 'Vote on question', '
   given(:my_question) { create(:question, user: user) }
   given(:question) { create(:question) }
 
-  context 'When authenticated' do
-    background do
-      sign_in(user)
-    end
-
-    scenario 'upvote on a question', js: true do
-      visit question_path(question)
-
-      within('#question') do
-        find(:css, '.upvote-link').trigger('click')
-        wait_for_ajax
-
-        expect(find('.vote')['class']).to include('upvoted')
-        expect(find('.vote')['class']).to_not include('downvoted')
-        expect(find('.score')).to have_content('1')
-      end
-    end
-
-    scenario 'unvote the question', js: true do
-      visit question_path(question)
-
-      within('#question') do
-        find(:css, '.upvote-link').trigger('click')
-        wait_for_ajax
-        find(:css, '.upvote-link').trigger('click')
-        wait_for_ajax
-
-        expect(find('.vote')['class']).to_not include('upvoted')
-        expect(find('.vote')['class']).to_not include('downvoted')
-        expect(find('.score')).to have_content('0')
-      end
-    end
-
-    scenario 'downvote a question', js: true do
-      visit question_path(question)
-
-      within('#question') do
-        find(:css, '.downvote-link').trigger('click')
-        wait_for_ajax
-
-        expect(find('.vote')['class']).to_not include('upvoted')
-        expect(find('.vote')['class']).to include('downvoted')
-        expect(find('.score')).to have_content('-1')
-      end
-    end
-
-    scenario 'downvote if already upvoted', js: true do
-      visit question_path(question)
-
-      within('#question') do
-        find(:css, '.upvote-link').trigger('click')
-        wait_for_ajax
-        find(:css, '.downvote-link').trigger('click')
-        wait_for_ajax
-
-        expect(find('.vote')['class']).to_not include('upvoted')
-        expect(find('.vote')['class']).to include('downvoted')
-        expect(find('.score')).to have_content('-1')
-      end
-    end
-
-    scenario 'upvote if already downvoted', js: true do
-      visit question_path(question)
-
-      within('#question') do
-        find(:css, '.downvote-link').trigger('click')
-        wait_for_ajax
-        find(:css, '.upvote-link').trigger('click')
-        wait_for_ajax
-
-        expect(find('.vote')['class']).to include('upvoted')
-        expect(find('.vote')['class']).to_not include('downvoted')
-        expect(find('.score')).to have_content('1')
-      end
-    end
-
-    scenario "can't upvote my own question", js: true do
-      visit question_path(my_question)
-
-      within('#question') do
-        find(:css, '.upvote-link').trigger('click')
-        wait_for_ajax
-
-        expect(find('.vote')['class']).to_not include('upvoted')
-        expect(find('.vote')['class']).to_not include('downvoted')
-        expect(find('.score')).to have_content('0')
-      end
-    end
-
-    scenario 'upvote when a question is already upvoted by another user', js: true do
-      create(:vote, user: another_user, votable: question)
-
-      visit question_path(question)
-
-      within('#question') do
-        find(:css, '.upvote-link').trigger('click')
-        wait_for_ajax
-
-        expect(find('.vote')['class']).to include('upvoted')
-        expect(find('.vote')['class']).to_not include('downvoted')
-        expect(find('.score')).to have_content('2')
-      end
-    end
+  it_behaves_like 'Giving vote on votable' do
+    let(:votable) { question }
+    let(:votable_locator) { '#question' }
   end
 
-  context 'When unauthenticated' do
-    scenario "can't vote on a question", js: true do
-      visit question_path(my_question)
+  scenario "can't upvote my own question", js: true do
+    sign_in(user)
+    visit question_path(my_question)
 
-      within('#question') do
-        find(:css, '.upvote-link').trigger('click')
-        wait_for_ajax
+    within('#question') do
+      find(:css, '.upvote-link').trigger('click')
+      wait_for_ajax
 
-        expect(find('.vote')['class']).to_not include('upvoted')
-        expect(find('.vote')['class']).to_not include('downvoted')
-        expect(find('.score')).to have_content('0')
-      end
+      expect(find('.vote')['class']).to_not include('upvoted')
+      expect(find('.vote')['class']).to_not include('downvoted')
+      expect(find('.score')).to have_content('0')
     end
   end
 end
