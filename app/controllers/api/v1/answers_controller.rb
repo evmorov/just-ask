212ae543674880068f9e811f8d9c41f3acb9f637
Evgeny Morozov <evmorov@gmail.com>
2017-02-04 17:03:12 +0300

    Refactor api controllers

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::AnswersController < Api::V1::BaseController
-  authorize_resource
-
   before_action :load_question, only: [:index, :create]
+
+  authorize_resource
 
   def index
     respond_with @question.answers
