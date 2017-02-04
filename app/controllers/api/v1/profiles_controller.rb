2d71bba64d9ffd139afce7c270166ba534506be7
Evgeny Morozov <evmorov@gmail.com>
2017-02-04 17:03:12 +0300

    Refactor api controllers

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::ProfilesController < Api::V1::BaseController
+  authorize_resource class: User
+
   def me
-    authorize! :me, User
     respond_with current_resource_owner
   end
 
   def index
-    authorize! :index, User
     respond_with User.where.not(id: current_resource_owner.id)
   end
 end
