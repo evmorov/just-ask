8d710230538bbe3c528dd49630246e3a17e0a1ba
Evgeny Morozov <evmorov@gmail.com>
2017-02-10 14:31:29 +0300

    Fix daily digest sending time

--- app/jobs/daily_digest_job.rb
+++ app/jobs/daily_digest_job.rb
@@ -2,7 +2,7 @@ class DailyDigestJob < ApplicationJob
   queue_as :mailers
 
   def perform
-    questions_ids = Question.where(created_at: 1.day.ago..Time.now).ids
+    questions_ids = Question.where(created_at: Date.yesterday.midnight..Date.today.midnight - 1).ids
     User.find_each do |user|
       DailyMailer.digest(user, questions_ids).deliver_later
     end
--- spec/acceptance/other/daily_digest_spec.rb
+++ spec/acceptance/other/daily_digest_spec.rb
@@ -7,8 +7,13 @@ feature 'Daily digest', '
 ' do
 
   given(:users) { create_list(:user, 2) }
-  given!(:questions) { create_list(:question, 2, user: users.first) }
-  given!(:old_questions) { create_list(:question, 2, user: users.first, created_at: 1.day.ago) }
+  given!(:questions_digest) { create_list(:question, 2, user: users.first, created_at: 1.day.ago) }
+  given!(:old_questions) do
+    create_list(:question, 2, user: users.first, created_at: (Date.yesterday.midnight - 1))
+  end
+  given!(:new_questions) do
+    create_list(:question, 2, user: users.first, created_at: Date.today.midnight)
+  end
 
   scenario 'Every user receives an email with all new questions' do
     DailyDigestJob.perform_now
@@ -16,13 +21,18 @@ feature 'Daily digest', '
     users.each do |user|
       open_email user.email
 
-      old_questions.each do |old_question|
-        expect(current_email).to_not have_link(old_question.title)
+      old_questions.each do |question|
+        expect(current_email).to_not have_link(question.title)
       end
 
-      questions.each do |question|
+      new_questions.each do |question|
+        expect(current_email).to_not have_link(question.title)
+      end
+
+      questions_digest.each do |question|
         expect(current_email).to have_link(question.title, href: question_url(question))
       end
+
       expect(all_emails.size).to eq(users.size)
     end
   end
