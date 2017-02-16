6b351c8e39d4de998b7b4a8498f41db1f0d84784
Evgeny Morozov <evmorov@gmail.com>
2017-02-16 10:44:39 +0300

    Fix unicorn problem with sockets

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,47 +1,44 @@
 # paths
-app_path = "/home/deployer/just-ask"
+app_path = '/home/deployer/just-ask'
 working_directory "#{app_path}/current"
 pid               "#{app_path}/current/tmp/pids/unicorn.pid"
 
 # listen
-listen "/tmp/unicorn.just-ask.sock", backlog: 64
+listen '/home/deployer/just-ask/shared/tmp/sockets/unicorn.just-ask.sock', backlog: 64
 
 # logging
-stderr_path "log/unicorn.stderr.log"
-stdout_path "log/unicorn.stdout.log"
+stderr_path 'log/unicorn.stderr.log'
+stdout_path 'log/unicorn.stdout.log'
 
 # workers
 worker_processes 2
 
 # use correct Gemfile on restarts
-before_exec do |server|
+before_exec do |_server|
   ENV['BUNDLE_GEMFILE'] = "#{app_path}/current/Gemfile"
 end
 
 # preload
 preload_app true
 
-before_fork do |server, worker|
+before_fork do |server, _worker|
   # the following is highly recomended for Rails + "preload_app true"
   # as there's no need for the master process to hold a connection
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
-  end
+  ActiveRecord::Base.connection.disconnect! if defined?(ActiveRecord::Base)
 
   # Before forking, kill the master process that belongs to the .oldbin PID.
   # This enables 0 downtime deploys.
   old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
+  if File.exist?(old_pid) && server.pid != old_pid
     begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
+      Process.kill('QUIT', File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH => e
       # someone else did our job for us
+      warn e
     end
   end
 end
 
-after_fork do |server, worker|
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
-  end
+after_fork do |_server, _worker|
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
 end
