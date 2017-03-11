ebbc7d82b3a423ed17dec05c2396f867e085842d
Evgeny Morozov <evmorov@gmail.com>
2017-03-11 21:12:52 +0300

    Refactor username

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,19 +1,14 @@
 class OauthsController < ApplicationController
   skip_authorization_check
 
-  def ask_email
-    @user = User.create_if_not_exist_w_auth(params['email'], params['provider'], params['uid'])
-
-    if @user.confirmed_at
-      @user.update(confirmed_at: nil)
-      @user.send_confirmation_instructions
-    end
-    redirect_to root_path
-  end
-
   def ask_username
-    @user = User.create_if_not_exist_w_auth(params['email'], params['username'], params['provider'], params['uid'])
-
+    auth = {
+      email: params['email'],
+      username: params['username'],
+      provider: params['provider'],
+      uid: params['uid']
+    }
+    @user = User.create_user_and_auth(auth)
     redirect_to root_path
   end
 end
