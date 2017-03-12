5d1592088320b391424f795211e3ce1d5939c78e
Evgeny Morozov <evmorov@gmail.com>
2017-03-12 19:22:39 +0300

    Add username to comments but not shown up with live update

--- spec/acceptance/comments/add_comment_to_answer_spec.rb
+++ spec/acceptance/comments/add_comment_to_answer_spec.rb
@@ -15,31 +15,4 @@ feature 'Add a comment to the answer', '
   it_behaves_like 'Comments' do
     let(:commentable_locator) { "#answer_#{answer.id}" }
   end
-
-  context 'Mulitple sessions', js: true do
-    scenario "comment appears on another user's page" do
-      Capybara.using_session('user') do
-        sign_in(user)
-        visit question_path(question)
-      end
-
-      Capybara.using_session('another_user') do
-        visit question_path(question)
-      end
-
-      Capybara.using_session('user') do
-        within("#answer_#{answer.id}") do
-          click_on 'Add a comment'
-          fill_in 'comment_body', with: 'This is a comment'
-          click_on 'Create Comment'
-        end
-      end
-
-      Capybara.using_session('another_user') do
-        within("#answer_#{answer.id}") do
-          expect(page).to have_content 'This is a comment'
-        end
-      end
-    end
-  end
 end
